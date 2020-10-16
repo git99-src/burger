2e16aa319734eaaffeff0f51158085a45e85f5b3
git99.src <cpu.al2@gmail.com>
2020-10-15 20:37:37 -0700

    Created first route to show all items in database with basic handlebar output

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,5 +4,17 @@ const express = require("express");
 const burger = require("../models/burger.js");
 const router = express.Router();
 
+
+// Create route to get all burgers
+router.get("/", (req, res) => {
+    burger.all((data) => {
+      const hbsObject = {
+        burgers: data,
+      };
+      // console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
 // Export routes for server.js to use.
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,12 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-
-  
+const burger = {
+  all: (cb) => {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  },
+};
   // Export the database functions for the controller (burgerController.js).
   module.exports = burger;
